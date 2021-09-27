e25ba198efe649a654b18d1e5c71f022a9747e6b
Mschuppers <m.schuppers@belastingdienst.nl>
2021-09-27 09:17:07 +0200

    Only need to fix option 3 for GoldMembers, system refuses to find cardId's of Goldmembers

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -9,25 +9,20 @@ public abstract class Card {
         return this.cardId;
     }
 
-
-
-
-
-
     //optional
     protected String address;
     protected String city;
 
     //Constructor with datamethods
-     abstract boolean createCard(int cardId, String name, double credit);
+    abstract boolean createCard(int cardId, String name, double credit);
     abstract boolean createCard(int cardId, String name, double credit, int discount);
 
 
     //Methods
     protected abstract boolean pay(double amount);
-    protected void printMyDetails() {
-        System.out.println(this.cardId + "      " + this.name + "      " + this.credit);
-    }
+//    protected void printMyDetails() {
+//        System.out.println(this.cardId + "      " + this.name + "      " + this.credit);
+//    }
     abstract Card searchAccount(int number);
 
 
